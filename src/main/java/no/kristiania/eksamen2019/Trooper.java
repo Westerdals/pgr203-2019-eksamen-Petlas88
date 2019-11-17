d0698710a8d1bbe8190fb8cdb3dc2793aea63b6a
Martinius222 <martinius.tomter@me.com>
2019-11-17 13:44:59 +0100

    Import trooper class, dao and controller from test project

---
+++ src/main/java/no/kristiania/eksamen2019/Trooper.java
@@ -0,0 +1,73 @@
+package no.kristiania.eksamen2019;
+
+import java.util.Objects;
+
+public class Trooper {
+
+
+    private long id;
+    private String name;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public Trooper() {
+
+
+    }
+
+    public Trooper(String name, String email) {
+        this.name = name;
+        this.email = email;
+
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Trooper trooper = (Trooper) o;
+        return id == trooper.id &&
+                Objects.equals(name, trooper.name) &&
+                Objects.equals(email, trooper.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Trooper{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
