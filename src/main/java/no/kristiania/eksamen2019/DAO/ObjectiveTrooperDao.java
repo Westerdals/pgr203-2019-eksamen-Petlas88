00d01c3b416f59414109b4ef51af394a889a9d7a
PanRagon <thebigecko@gmail.com>
2019-11-17 21:17:02 +0100

    Add new ObjectiveTrooper functionality :hotsprings:

--- src/main/java/no/kristiania/eksamen2019/DAO/ObjectiveTrooperDao.java
+++ src/main/java/no/kristiania/eksamen2019/DAO/ObjectiveTrooperDao.java
@@ -9,53 +9,33 @@ import java.util.List;
 
 public class ObjectiveTrooperDao extends AbstractDao<ObjectiveTrooper> {
     public ObjectiveTrooperDao(DataSource dataSource) {
-        super(dataSource);
+        super( dataSource );
     }
-
 
     @Override
     protected void insertMember(ObjectiveTrooper objectiveTrooper, PreparedStatement statement) throws SQLException {
-        statement.setString(1, objectiveTrooper.getName());
-        statement.setString(2, objectiveTrooper.getEmail());
-
+        statement.setInt( 1, objectiveTrooper.getTrooperId() );
+        statement.setInt( 2, objectiveTrooper.getObjectiveId() );
     }
 
     @Override
     protected ObjectiveTrooper readObject(ResultSet resultSet) throws SQLException {
         ObjectiveTrooper objectiveTrooper = new ObjectiveTrooper();
-
-        objectiveTrooper.setId(resultSet.getInt(1));
-        objectiveTrooper.setName(resultSet.getString(2));
-        objectiveTrooper.setEmail(resultSet.getString(3));
+        objectiveTrooper.setObjectiveId(resultSet.getInt( 2 ) );
+        objectiveTrooper.setTrooperId( resultSet.getInt( 1 ) );
         return objectiveTrooper;
     }
 
-
-    public long insert(ObjectiveTrooper ObjectiveTrooper) throws SQLException {
-        long id = insert(ObjectiveTrooper, "insert into ObjectiveTroopers (name,email) values (?,?)");
-        ObjectiveTrooper.setId(id);
-        return id;
-    }
-
     public List<ObjectiveTrooper> listAll() throws SQLException {
-        return listAll("select * from ObjectiveTroopers");
+        return listAll( "select * from objectivetroopers" );
     }
 
-    public ObjectiveTrooper retrieve(long id) throws SQLException{
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("select * from ObjectiveTroopers where id = ?")) {
-                statement.setLong(1, id);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if(resultSet.next()) {
-                        return (readObject(resultSet));
-                    } else {
-                        return null;
-                    }
-                }
-            }
+    public void insert(ObjectiveTrooper objectiveTrooper) throws SQLException {
+        //checks if object already exist in database before inserting
+        if (this.listAll().contains( objectiveTrooper )) {
+            System.out.println( "FINNES ALEREDE!" );
+            return;
         }
+        insert( objectiveTrooper, "insert into objectivetroopers (member_id,task_id) values (?,?)" );
     }
-
-
-
 }
